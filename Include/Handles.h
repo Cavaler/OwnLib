4dd723b2d3387e4fd123cb9e81ab21682f08f65d
Cavaler <cavaler.pk@gmail.com>
2007-10-12 09:23:34 +0000

    .Close()

--- Include/Handles.h
+++ Include/Handles.h
@@ -1,12 +1,23 @@
 #ifndef __HANDLES_H
 #define __HANDLES_H
 
-template<class HandleType, int Null = (int)INVALID_HANDLE_VALUE, void *Close = CloseHandle> class CHandleTempl {
+template<class HandleType, int Null = (int)INVALID_HANDLE_VALUE, void *CloseFn = CloseHandle> class CHandleTempl {
 public:
 	CHandleTempl() : m_hHandle((HandleType)Null) {}
 	CHandleTempl(HandleType handle) : m_hHandle(handle) {}
-	void operator =(HandleType handle) {m_hHandle = handle;}
-	~CHandleTempl() {if (m_hHandle != (HandleType)Null) ((CloseFunc)Close)(m_hHandle);}
+	void operator =(HandleType handle) {
+		if (m_hHandle != handle) {
+			Close();
+			m_hHandle = handle;
+		}
+	}
+	void Close() {
+		if (m_hHandle != (HandleType)Null) {
+			((CloseFunc)CloseFn)(m_hHandle);
+			m_hHandle = (HandleType)Null;
+		}
+	}
+	~CHandleTempl() {Close();}
 
 	HandleType *operator &() {return &m_hHandle;}
 	operator HandleType &() {return m_hHandle;}
