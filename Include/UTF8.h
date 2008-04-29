6658004461c098ca9a420fcdf0f62601e6bd7023
Cavaler <cavaler.pk@gmail.com>
2008-04-29 15:02:01 +0000

    std::

--- Include/UTF8.h
+++ Include/UTF8.h
@@ -4,13 +4,13 @@
 #include <string>
 
 std::string EncodeUTF8(const wchar_t *pwszValue, int nLength = -1);
-std::string EncodeUTF8(const wstring &wstrValue);
+std::string EncodeUTF8(const std::wstring &wstrValue);
 std::string EncodeUTF8(const char *pszValue, int nLength = -1, unsigned int uiCP = CP_ACP);
-std::string EncodeUTF8(const string &strValue, unsigned int uiCP = CP_ACP);
+std::string EncodeUTF8(const std::string &strValue, unsigned int uiCP = CP_ACP);
 
 std::wstring DecodeUTF8(const char *pszValue, int nLength = -1);
-std::wstring DecodeUTF8(const string &strValue);
+std::wstring DecodeUTF8(const std::string &strValue);
 std::string DecodeUTF8A(const char *pszValue, int nLength = -1, unsigned int uiCP = CP_ACP);
-std::string DecodeUTF8A(const string &strValue, unsigned int uiCP = CP_ACP);
+std::string DecodeUTF8A(const std::string &strValue, unsigned int uiCP = CP_ACP);
 
 #endif
