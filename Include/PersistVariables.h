4254b1e58a3e611ac93803c863ebe90b318ba040
Cavaler <cavaler.pk@gmail.com>
2008-07-10 16:17:39 +0000

    PersistVars with explicit key name

--- Include/PersistVariables.h
+++ Include/PersistVariables.h
@@ -4,6 +4,12 @@
 	#define PERSIST_STRING_VARIABLE(Name, Default) EXTERN string Name;
 	#define PERSIST_TYPED_VARIABLE(Type, Name, Default, Min, Max) EXTERN Type Name VALUE(Default);
 	#define PERSIST_FILETIME_VARIABLE(Name) EXTERN FILETIME Name;
+	
+	#define PERSIST_BOOL_VARIABLE_(Name, Key, Default) EXTERN BOOL Name VALUE(Default);
+	#define PERSIST_bool_VARIABLE_(Name, Key, Default) EXTERN bool Name VALUE(Default);
+	#define PERSIST_STRING_VARIABLE_(Name, Key, Default) EXTERN string Name;
+	#define PERSIST_TYPED_VARIABLE_(Type, Name, Key, Default, Min, Max) EXTERN Type Name VALUE(Default);
+	#define PERSIST_FILETIME_VARIABLE_(Name, Key) EXTERN FILETIME Name;
 
 #elif defined DECLARE_PERSIST_LOAD
 	#define PERSIST_BOOL_VARIABLE(Name, Default) QueryRegBoolValue(DECLARE_PERSIST_LOAD, #Name, &Name, Default);
@@ -12,12 +18,24 @@
 	#define PERSIST_TYPED_VARIABLE(Type, Name, Default, Min, Max) QueryRegIntValue(DECLARE_PERSIST_LOAD, #Name, (int *)&Name, Default, Min, Max);
 	#define PERSIST_FILETIME_VARIABLE(Name) QueryRegIntValue(DECLARE_PERSIST_LOAD, #Name "Lo", &Name.dwLowDateTime, 0xE1D58000); QueryRegIntValue(DECLARE_PERSIST_LOAD, #Name "Hi", &Name.dwHighDateTime, 0x01A8E79F);
 
+	#define PERSIST_BOOL_VARIABLE_(Name, Key, Default) QueryRegBoolValue(DECLARE_PERSIST_LOAD, Key, &Name, Default);
+	#define PERSIST_bool_VARIABLE_(Name, Key, Default) QueryRegBoolValue(DECLARE_PERSIST_LOAD, Key, &Name, Default);
+	#define PERSIST_STRING_VARIABLE_(Name, Key, Default) QueryRegStringValue(DECLARE_PERSIST_LOAD, Key, Name, Default);
+	#define PERSIST_TYPED_VARIABLE_(Type, Name, Key, Default, Min, Max) QueryRegIntValue(DECLARE_PERSIST_LOAD, Key, (int *)&Name, Default, Min, Max);
+	#define PERSIST_FILETIME_VARIABLE_(Name, Key) QueryRegIntValue(DECLARE_PERSIST_LOAD, Key "Lo", &Name.dwLowDateTime, 0xE1D58000); QueryRegIntValue(DECLARE_PERSIST_LOAD, Key "Hi", &Name.dwHighDateTime, 0x01A8E79F);
+
 #elif defined DECLARE_PERSIST_SAVE
 	#define PERSIST_BOOL_VARIABLE(Name, Default) SetRegBoolValue(DECLARE_PERSIST_SAVE, #Name, Name);
 	#define PERSIST_bool_VARIABLE(Name, Default) SetRegBoolValue(DECLARE_PERSIST_SAVE, #Name, Name);
 	#define PERSIST_STRING_VARIABLE(Name, Default) SetRegStringValue(DECLARE_PERSIST_SAVE, #Name, Name);
 	#define PERSIST_TYPED_VARIABLE(Type, Name, Default, Min, Max) SetRegIntValue(DECLARE_PERSIST_SAVE, #Name, Name);
 	#define PERSIST_FILETIME_VARIABLE(Name) SetRegIntValue(DECLARE_PERSIST_SAVE, #Name "Lo", Name.dwLowDateTime); SetRegIntValue(DECLARE_PERSIST_SAVE, #Name "Hi", Name.dwHighDateTime);
+
+	#define PERSIST_BOOL_VARIABLE_(Name, Key, Default) SetRegBoolValue(DECLARE_PERSIST_SAVE, Key, Name);
+	#define PERSIST_bool_VARIABLE_(Name, Key, Default) SetRegBoolValue(DECLARE_PERSIST_SAVE, Key, Name);
+	#define PERSIST_STRING_VARIABLE_(Name, Key, Default) SetRegStringValue(DECLARE_PERSIST_SAVE, Key, Name);
+	#define PERSIST_TYPED_VARIABLE_(Type, Name, Key, Default, Min, Max) SetRegIntValue(DECLARE_PERSIST_SAVE, Key, Name);
+	#define PERSIST_FILETIME_VARIABLE_(Name, Key) SetRegIntValue(DECLARE_PERSIST_SAVE, Key "Lo", Name.dwLowDateTime); SetRegIntValue(DECLARE_PERSIST_SAVE, Key "Hi", Name.dwHighDateTime);
 #else
 	#error Don't know what to do
 #endif
