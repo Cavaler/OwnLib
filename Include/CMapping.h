2a8ebfc2925341149b3c9e9ee7ee1e4663c0bc10
Cavaler <cavaler.pk@gmail.com>
2008-07-23 13:46:21 +0000

    Sequential reading

--- Include/CMapping.h
+++ Include/CMapping.h
@@ -8,6 +8,10 @@
 class CFileMapping {
 public:
 	CFileMapping();
+	CFileMapping(const char *szFileName);
+	operator bool();
+	bool operator !();
+
 	void *Open(const char *szFileName);
 	operator void *();
 	operator BYTE *();
@@ -17,10 +21,24 @@ public:
 	DWORD Size();
 	void Close();
 	~CFileMapping();
+
+	DWORD Pos();
+	DWORD Left();
+	bool  Seek(DWORD dwPos);
+	bool  Forward(DWORD dwPos);
+
+	bool Read(void *pValue, DWORD dwCount);
+	template<class T> bool Read(T &Value) {
+		return Read(&Value, sizeof(Value));
+	}
+	int ReadInt();
+
 protected:
 	HANDLE m_hFile;
 	HANDLE m_hMapping;
 	void *m_pData;
+
+	DWORD  m_dwPos;
 };
 
 #endif
--- LibSrc/CMapping.cpp
+++ LibSrc/CMapping.cpp
@@ -1,6 +1,18 @@
 #include "CMapping.h"
 
-CFileMapping::CFileMapping() : m_hFile(INVALID_HANDLE_VALUE), m_hMapping(NULL), m_pData(NULL) {
+CFileMapping::CFileMapping() : m_hFile(INVALID_HANDLE_VALUE), m_hMapping(NULL), m_pData(NULL), m_dwPos(0) {
+}
+
+CFileMapping::CFileMapping(const char *szFileName) : m_hMapping(NULL), m_pData(NULL), m_dwPos(0) {
+	Open(szFileName);
+}
+
+CFileMapping::operator bool() {
+	return m_pData != NULL;
+}
+
+bool CFileMapping::operator !() {
+	return m_pData == NULL;
 }
 
 void *CFileMapping::Open(const char *szFileName) {
@@ -23,6 +35,8 @@ void *CFileMapping::Open(const char *szFileName) {
 		CloseHandle(m_hFile);m_hFile = INVALID_HANDLE_VALUE;
 		return NULL;
 	}
+
+	m_dwPos = 0;
 	return m_pData;
 }
 
@@ -50,10 +64,46 @@ DWORD CFileMapping::Size() {
 	return (m_hFile) ? GetFileSize(m_hFile, NULL) : 0;
 }
 
+DWORD CFileMapping::Pos() {
+	return m_dwPos;
+}
+
+DWORD CFileMapping::Left() {
+	return Size()-m_dwPos;
+}
+
+bool CFileMapping::Seek(DWORD dwPos) {
+	if (dwPos > Size()) return false;
+	m_dwPos = dwPos;
+	return true;
+}
+
+bool CFileMapping::Forward(DWORD dwPos) {
+	if (dwPos > Left()) return false;
+	m_dwPos += dwPos;
+	return true;
+}
+
+bool CFileMapping::Read(void *pValue, DWORD dwCount) {
+	if (dwCount > Left()) return false;
+
+	if (pValue) memmove(pValue, (BYTE *)m_pData + m_dwPos, dwCount);
+	m_dwPos += dwCount;
+
+	return true;
+}
+
+int CFileMapping::ReadInt() {
+	int nResult = 0;
+	Read(nResult);
+	return nResult;
+}
+
 void CFileMapping::Close() {
 	if (m_pData) {UnmapViewOfFile(m_pData);m_pData = NULL;}
 	if (m_hMapping != NULL) {CloseHandle(m_hMapping);m_hMapping = NULL;}
 	if (m_hFile != INVALID_HANDLE_VALUE) {CloseHandle(m_hFile);m_hFile = INVALID_HANDLE_VALUE;}
+	m_dwPos = 0;
 }
 
 CFileMapping::~CFileMapping() {
