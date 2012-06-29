0fea6af6614794286a359cf4f6267f824b6fa0bb
Cavaler <cavaler.pk@gmail.com>
2012-06-29 16:03:11 +0000

    Compileable LibExif

---
+++ LibSrc/libexif-0.6.20/libexif/_stdint.h
@@ -0,0 +1,9 @@
+/* This file is generated automatically by configure */
+
+typedef unsigned short uint16_t;
+typedef unsigned short int16_t;
+
+typedef unsigned long uint32_t;
+typedef unsigned long int32_t;
+
+typedef int  ssize_t;
