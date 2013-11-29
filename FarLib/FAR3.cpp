6093908790cbf0f7d7a7566391bbf577a3dd2aac
Cavaler <cavaler.pk@gmail.com>
2013-11-29 14:08:45 +0000

    Another message OK

--- FarLib/FAR3.cpp
+++ FarLib/FAR3.cpp
@@ -178,6 +178,7 @@ intptr_t CPluginStartupInfo::EditorControl(int Command, void *Param)
 	case ECTL_UNDOREDO:
 	case ECTL_INSERTSTRING:
 	case ECTL_DELETESTRING:
+	case ECTL_PROCESSINPUT:
 		return __super::EditorControl(-1, fCommand, 0, Param);
 	default:
 		assert(0);
