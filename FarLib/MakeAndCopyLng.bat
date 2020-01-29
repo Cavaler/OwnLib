7b01c4c8054aa6e19bad84af6303341e53eb6f04
Cavaler <pavel@kostrom.spb.ru>
2020-01-29 19:33:49 +0300

    Fixed building msg

--- FarLib/MakeAndCopyLng.bat
+++ FarLib/MakeAndCopyLng.bat
@@ -1,6 +1,12 @@
 @echo off
-call %~dp0\MakeLng.bat %$
+call %~dp0\MakeLng.bat %*
 echo Copying resulting language files...
-copy *.lng "%FAR_DEBUG_DLLS%\%1\" 2>nul
-copy *.lng "%FAR2_DEBUG_DLLS%\%1\" 2>nul
-copy *.lng "%FAR3_DEBUG_DLLS%\%1\" 2>nul
+if NOT "%FAR_DEBUG_DLLS%"==""(
+  copy *.lng "%FAR_DEBUG_DLLS%\%1\" 2>nul
+)
+if NOT "%FAR2_DEBUG_DLLS%"==""(
+  copy *.lng "%FAR2_DEBUG_DLLS%\%1\" 2>nul
+)
+if NOT "%FAR3_DEBUG_DLLS%"==""(
+  copy *.lng "%FAR3_DEBUG_DLLS%\%1\" 2>nul
+)
